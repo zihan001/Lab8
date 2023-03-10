a300022af0d8f9a0e6b7d32821f459afabca8b21
“Zihan <zihanhossain99@gmail.com>
2023-03-09 18:00:42 -0700

    hasCity Test will Fail

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -53,4 +53,9 @@ public class CustomList extends ArrayAdapter<City> {
         cities.add(city);
     }
 
+    public boolean hasCity(City city) {
+
+    }
+
+
 }
--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,8 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -32,5 +34,20 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+
+    @Test
+    void testHasCity() {
+        list = MockCityList();
+        City city = new City("Yellowknife", "Northwest Territories");
+        list.add(city);
+        assertTrue(list.hasCity(city));
+    }
+
+    @Test
+    void testNotHasCity() {
+        list = MockCityList();
+        City city = new City("Yellowknife", "Northwest Territories");
+        assertFalse(list.hasCity(city));
     }
 }
