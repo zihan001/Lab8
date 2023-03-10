2efd11be136553d3ce916fc8ddbe35737e8d1ccf
“Zihan <zihanhossain99@gmail.com>
2023-03-09 18:21:58 -0700

    delete Test will Pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -58,7 +58,11 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public void delete(City city) {
-
+        if (cities.contains(city)) {
+            cities.remove(city);
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
 }
